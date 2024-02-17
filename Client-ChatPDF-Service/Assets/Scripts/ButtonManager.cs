3cf5df3bd0e3da842933cdfb3a0e8321224c6f4c
Lee Ui Hwan <luh5063014@naver.com>
2024-02-17 14:05:45 +0900

    Chore: 버튼 클릭 시 react-unity 연동 버그 수정

--- Client-ChatPDF-Service/Assets/Scripts/ButtonManager.cs
+++ Client-ChatPDF-Service/Assets/Scripts/ButtonManager.cs
@@ -2,56 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
-    private int score = 0;
-
-    [SerializeField]
-    private TextMeshProUGUI sampleText;
-
-    [SerializeField]
-    private GameObject sendButton;
-
-    [SerializeField]
-    private GameObject listenButton;
-
-    public int Score
+    public void SetObject(GameObject obj)
     {
-        get
+        if (obj.gameObject.activeSelf)
         {
-            return score;
-        }
-        set
-        {
-            score = value;
-            sampleText.text = score.ToString();
-            Debug.Log(score);
-        }
-    }
-
-    public void BtnClick()
-    {
-        Score += 1;
-    }
-
-    // Button Active On/Off
-    public void SetButton(GameObject button)
-    {
-        if(button.gameObject.activeSelf)
-        {
-            button.gameObject.SetActive(false);
-            listenButton.SetActive(true);
-            sendButton.SetActive(false);
+            obj.gameObject.SetActive(false);
         }
         else
         {
-            button.gameObject.SetActive(true);
-            listenButton.SetActive(false);
-            sendButton.SetActive(true);
+            obj.gameObject.SetActive(true);
         }
     }
+
 
     // Start is called before the first frame update
     void Start()
