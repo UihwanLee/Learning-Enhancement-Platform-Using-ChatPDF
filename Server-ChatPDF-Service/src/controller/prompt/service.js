628ca1c8c78e8b351166616512d8e90b9d81866c
SeHyeonLee-dev <sktpgus2023@gmail.com>
2024-06-14 21:33:11 +0900

    Feat: generateDetailQuestions 함수 추가

--- Server-ChatPDF-Service/src/controller/prompt/service.js
+++ Server-ChatPDF-Service/src/controller/prompt/service.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const { getDocumentPath } = require('../files/documentStore');
 
-
+// [사전 조사] 초기 질문 5개 생성
 async function generateQuestions(numQuestions) {
   const db = await connectDB();
   const filePath = getDocumentPath(); 
@@ -34,37 +34,23 @@ async function generateQuestions(numQuestions) {
   return questions;
 }
 
-async function generateDetailQuestions(numQuestions) {
-  //const filePath = path.join(__dirname, "algo.pdf");
+// [면접 진행] index에 따라 질문 3개 생성
+async function generateDetailQuestions(numQuestions, category) {
   const filePath = getDocumentPath(); 
-  console.log("filePath: ", filePath);
-  const prompt = `이 pdf 파일을 참조해서 이 주제와 관련해서 나의 학습 수준을 파악하기 위한 주관식 문제 ${numQuestions}개를 직접 만들어서 내줘. 다른 말은 하지말고 문제만 말해주고 말 끝은 반드시 ! 하나를 넣어줘.`;
+  const prompt = `이 pdf 파일을 참조해서 ${category}와 관련된 지식 수준을 파악하기 위한 주관식 문제 ${numQuestions}개를 직접 만들어서 내줘. 
+  다른 말은 하지말고 문제만 말해주고 말 끝은 반드시 ! 하나를 넣어줘.`;
 
   const question = await chatPDF(filePath, prompt);
   const split_questions = question.split('!');
   split_questions.pop();
   const questions = split_questions.map(item => item.trim());
 
-  const db = await connectDB();
-  for (let i = 0; i < questions.length; i++) {
-    await db.collection('prompt').insertOne({ user: null, question: questions[i], answer: null, score: null, General_opinion: null, Model_answer: null});
-  }
-
-  return questions;
-}
-
-async function generateTailQuestion(questionPrompt) {
-  const filePath = path.join(__dirname, "algo.pdf");
-  const prompt = "주어진 질문 '${questionPrompt}' 에 대해 질문과 직접적인 관련이 있는 꼬리 질문 1개를 생성해.";
-
-  const question = await chatPDF(filePath, prompt);
-
   // const db = await connectDB();
-  // for (let i = 0; i < tailQuestions.length; i++) {
-  //   await db.collection('tailQuestions').insertOne({ userID: null, question: tailQuestions[i], answer: null, score: null, General_opinion: null, Model_answer: null});
+  // for (let i = 0; i < questions.length; i++) {
+  //   await db.collection('prompt').insertOne({ user: null, question: questions[i], answer: null, score: null, General_opinion: null, Model_answer: null});
   // }
 
-  return tailQuestion;
+  return questions;
 }
 
 async function updateAnswer(answer) {
